e3412980523266906d8036de123dd97cc635eae7
YashilaPuthran <y.puthran01@gmail.com>
2015-09-21 16:43:26 -0400

    ydp: modified code for saving family details

--- Gedcom/src/com/stevens/cs555/gedcomProcessor.java
+++ Gedcom/src/com/stevens/cs555/gedcomProcessor.java
@@ -1,14 +1,4 @@
-/**
-* The gedcomProcessor class implements a class that can read, parse 
-* and validate GEDCOM data.
-*
-* @author  Durrani Rahul ,Monica Shukla,Ishani Desai,
-* @version 1.0
-* @since   2015-09-14 
-* @course  CS555
-*/
-
-//package com.stevens.cs555;
+package com.stevens.cs555;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -24,8 +14,8 @@ public class gedcomProcessor {
 	{
 		gedcomData line= new gedcomData();
 		String arg=new String();
-		String [] splitted=  currentline.split("\\s+"); 
-		
+		String [] splitted=  currentline.split("\\s+");
+
 		line.setLevel(splitted[0]);
 		if(splitted[0].equalsIgnoreCase("0"))
 		{
@@ -57,12 +47,55 @@ public class gedcomProcessor {
 			}
 		}
 		line.setArgument(arg);
-		
+
 		return line;
+	}
+	public static Family[] saveFamilyData(String path)
+	{
+		Family f = new Family();
+		int i=0;
+		Family a[] = new Family[10];
+		gedcomData tline,gline = new gedcomData();
+		String tLine,line;
+		BufferedReader br= null;
+		try {
+			br = new BufferedReader(new FileReader(path));
+			while((tLine = br.readLine()) != null)
+			{
+				tline= parseline(tLine);
+				if(tline.getLevel().equalsIgnoreCase("0")&& tline.getTag().equalsIgnoreCase("FAM"))
+				{
+					f=new Family();
+					f.setId(tline.getXrefid());
+					while((line=br.readLine())!=null)
+					{
+						gline = parseline(line);
+							if(gline.getTag().equalsIgnoreCase("HUSB"))
+							{
+								f.setHusband(gline.getArgument());
+							}
+							if(gline.getTag().equalsIgnoreCase("WIFE"))
+							{
+								f.setWife(gline.getArgument());
+								break;
+							}
+					}
+					a[i]=f;
+					i++;
+					}
+
+				}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return a;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Hashtable <String,String> tags = new Hashtable<String,String>();
+		Family family[] = new Family[1000];
 
 		tags.put("INDI", "0");
 		tags.put("NAME", "1");
@@ -82,18 +115,26 @@ public class gedcomProcessor {
 		tags.put("TRLR", "15");
 		tags.put("NOTE", "16");
 
+		Hashtable <String,String> Ind = new Hashtable<String,String>();
+
+
 		BufferedReader br= null;
 		try
 		{
 			String sCurrentLine;
 
 			br = new BufferedReader(new FileReader(args[0]));
-
 			while ((sCurrentLine = br.readLine()) != null) {
 				line=parseline(sCurrentLine);
+				if(line.getLevel().equalsIgnoreCase("0") && line.getTag().equalsIgnoreCase("INDI"))
+				{
+					Ind.put(line.getXrefid(), line.getArgument());
+				}
+
 				System.out.println("---------------------------------------------");
 				System.out.println(line.getLevel()+" "+line.getXrefid()+" "+line.getTag()+" "+line.getArgument());
 				System.out.println("Level: "+ line.getLevel());
+
 				if(tags.containsKey(line.getTag()))
 				{
 					System.out.println("Tag: "+ line.getTag());
@@ -102,9 +143,9 @@ public class gedcomProcessor {
 				{
 					System.out.println("Tag: Invalid");
 				}
-				
+
 				file.add(line);
-				
+
 			}
 		}catch (IOException e) {
 			e.printStackTrace();
@@ -113,7 +154,8 @@ public class gedcomProcessor {
 				if (br != null)br.close();
 			} catch (IOException ex) {
 				ex.printStackTrace();
-			} 
+			}
 	}
+		family = saveFamilyData(args[0]);
 	}
 }
