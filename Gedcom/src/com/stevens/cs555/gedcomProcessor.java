c584ef21cca7713b82dadbc840c950c0536f08a9
rahuldurrani <rdurrani@stevens.edu>
2015-09-22 01:48:29 -0400

    rd:Updated code for P03 which prints out families and individuals

--- Gedcom/src/com/stevens/cs555/gedcomProcessor.java
+++ Gedcom/src/com/stevens/cs555/gedcomProcessor.java
@@ -2,18 +2,20 @@
 * The gedcomProcessor class implements a class that can read, parse 
 * and validate GEDCOM data.
 *
-* @author  Durrani Rahul ,Monica Shukla,Ishani Desai,
+* @author  Durrani Rahul
 * @version 1.0
 * @since   2015-09-14 
 * @course  CS555
 */
 
-//package com.stevens.cs555;
+package com.stevens.cs555;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Enumeration;
 import java.util.Hashtable;
 
 public class gedcomProcessor {
@@ -60,10 +62,90 @@ public class gedcomProcessor {
 		
 		return line;
 	}
+	public static ArrayList<Family> saveFamilyData(String path)
+	{
+		Family f = new Family();
+		ArrayList<Family> a = new ArrayList<Family>();
+		gedcomData tline,gline = new gedcomData();
+		String tLine,line;
+		BufferedReader br= null;
+		try {
+			br = new BufferedReader(new FileReader(path));
+			while((tLine = br.readLine()) != null)
+			{
+				tline= parseline(tLine);
+				if(tline.getLevel().equalsIgnoreCase("0")&& tline.getTag().equalsIgnoreCase("FAM"))
+				{
+					f=new Family();
+					f.setId(tline.getXrefid());
+					while((line=br.readLine())!=null)
+					{
+						gline = parseline(line);
+							if(gline.getTag().equalsIgnoreCase("HUSB"))
+							{
+								f.setHusband(gline.getArgument());
+							}
+							if(gline.getTag().equalsIgnoreCase("WIFE"))
+							{
+								f.setWife(gline.getArgument());
+								break;
+							}
+					}
+					a.add(f);
+					}
+					
+				}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return a;
+	}
+	
+	public static Hashtable <String,String> saveIndiData(String path)
+	{
+		Hashtable <String,String> ind = new Hashtable<String,String>();
+		gedcomData tline,gline = new gedcomData();
+		String tLine,line;
+		String id = new String();
+		String name = new String();
+		BufferedReader br= null;
+		try {
+			br = new BufferedReader(new FileReader(path));
+			while((tLine = br.readLine()) != null)
+			{
+				tline= parseline(tLine);
+				if(tline.getLevel().equalsIgnoreCase("0")&& tline.getTag().equalsIgnoreCase("INDI"))
+				{
+					id=tline.getXrefid();
+					while((line=br.readLine())!=null)
+					{
+						gline = parseline(line);
+							if(gline.getTag().equalsIgnoreCase("NAME"))
+							{
+								name=gline.getArgument();
+								break;
+							}
+					}
+					ind.put(id, name);
+					}
+					
+				}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return ind;
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Hashtable <String,String> tags = new Hashtable<String,String>();
-
+		ArrayList<Family> family = new  ArrayList<Family>();
+       
 		tags.put("INDI", "0");
 		tags.put("NAME", "1");
 		tags.put("SEX", "2");
@@ -81,6 +163,9 @@ public class gedcomProcessor {
 		tags.put("HEAD", "14");
 		tags.put("TRLR", "15");
 		tags.put("NOTE", "16");
+		
+		Hashtable <String,String> Ind = new Hashtable<String,String>();
+		
 
 		BufferedReader br= null;
 		try
@@ -88,19 +173,20 @@ public class gedcomProcessor {
 			String sCurrentLine;
 
 			br = new BufferedReader(new FileReader(args[0]));
-
 			while ((sCurrentLine = br.readLine()) != null) {
 				line=parseline(sCurrentLine);
-				System.out.println("---------------------------------------------");
-				System.out.println(line.getLevel()+" "+line.getXrefid()+" "+line.getTag()+" "+line.getArgument());
-				System.out.println("Level: "+ line.getLevel());
+				
+				//System.out.println("---------------------------------------------");
+				//System.out.println(line.getLevel()+" "+line.getXrefid()+" "+line.getTag()+" "+line.getArgument());
+				//System.out.println("Level: "+ line.getLevel());
+				
 				if(tags.containsKey(line.getTag()))
 				{
-					System.out.println("Tag: "+ line.getTag());
+					//System.out.println("Tag: "+ line.getTag());
 				}
 				else
 				{
-					System.out.println("Tag: Invalid");
+					//System.out.println("Tag: Invalid");
 				}
 				
 				file.add(line);
@@ -115,5 +201,35 @@ public class gedcomProcessor {
 				ex.printStackTrace();
 			} 
 	}
+		family = saveFamilyData(args[0]);
+		Ind=saveIndiData(args[0]);
+		Enumeration items = Ind.keys();
+
+		// Now iterate
+		while(items.hasMoreElements()){
+			String key = (String)items.nextElement();
+		     System.out.println(key);
+		     System.out.println(Ind.get(key));
+		}
+		Ind=saveIndiData(args[0]);
+		for(Family str : family) 
+		{
+			System.out.println(str.getId());
+			Enumeration key = Ind.keys();
+			while(key.hasMoreElements())
+			{
+				String x = (String)key.nextElement();
+				if(str.getHusband().contains(x))
+				{
+					System.out.println("Husband:" + Ind.get(x));
+				}
+				if(str.getWife().contains(x))
+				{
+					System.out.println("Wife: "+ Ind.get(x));
+				}
+			}
+			
+		}
+
 	}
 }
